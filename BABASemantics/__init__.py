4f304201eaf239ae5695053994ca5d19fe4a4a06
Tomasz Szymaniec <tomek.szymaniec@gmail.com>
2017-06-06 17:04:58 +0100

    First commit

---
+++ BABASemantics/__init__.py
@@ -0,0 +1,5 @@
+from BABASemantics.Parser import BABAProgramParser
+from BABASemantics.Semantics import admissible, preferred, sceptically_preferred, complete, grounded, ideal, stable
+from BABASemantics.Semantics import GROUNDED, SCEPTICALLY_PREFERRED, IDEAL
+from BABASemantics.Semantics import compute_semantic_probability as semantic_probability
+from BABASemantics.Semantics import compute_semantic_probabilities as semantic_probabilities
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'BABASemantics',
+  packages = ['BABASemantics'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Parsing and computation of semantics for ABA and BABA frameworks',
+  author = 'Tomasz Szymaniec',
+  author_email = 'tomek.szymaniec@gmail.com',
+  url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
+  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
